12b6367fe5d0edce1e68ea9faf6e039a3c9c863a
KovacsBende <kovacs.bende.2003@gmail.com>
2025-04-24 23:25:18 +0200

    ahaű

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
-// Adatbázis kapcsolat beállítások
+
 $host = 'localhost';
 $port = '1521';
 $sid = 'XE';
 $username = 'KRISZTINA';
 $password = 'KRISZTINA';
 
-// Csatlakozás az Oracle adatbázishoz
+
 $conn = oci_connect($username, $password, "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))(CONNECT_DATA=(SID=$sid)))");
 
 if (!$conn) {
